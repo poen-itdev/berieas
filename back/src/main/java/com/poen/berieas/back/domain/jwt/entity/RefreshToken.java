e7e81bbfe4a3851c8fe6b27251e6f4edf746f1ca
김우재 <wjkim@poen.co.kr>
2025-08-21 14:56:23 +0900

    jwt 설정 완료

---
+++ back/src/main/java/com/poen/berieas/back/domain/jwt/entity/RefreshToken.java
@@ -0,0 +1,40 @@
+package com.poen.berieas.back.domain.jwt.entity;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "refresh_token")
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RefreshToken {
+    
+    @Id
+    private Long id;
+
+    @Column(name = "member_id", nullable = false)
+    private String memberId;
+
+    @Column(name = "refresh", nullable = false)
+    private String refresh;
+
+    @CreatedDate
+    @Column(name = "created_date", updatable = false)
+    private LocalDateTime createdDate;
+
+}
---
+++ back/src/main/java/com/poen/berieas/back/domain/jwt/service/JwtService.java
@@ -0,0 +1,48 @@
+package com.poen.berieas.back.domain.jwt.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.poen.berieas.back.domain.jwt.entity.RefreshToken;
+import com.poen.berieas.back.domain.jwt.repository.RefreshRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class JwtService {
+    
+    private final RefreshRepository refreshRepository;
+
+    // Jwt Refresh 토큰 발급 후 저장 메소드
+    @Transactional
+    public void addRefresh(String memberId, String refreshToken) {
+
+        RefreshToken token = RefreshToken.builder()
+            .memberId(memberId)
+            .refresh(refreshToken)
+            .build();
+
+        refreshRepository.save(token);
+    }
+
+    // Jwt Refresh 토큰 기반 존재 확인 메소드
+    public Boolean existsByRefresh(String refreshToken) {
+
+        return refreshRepository.existsByRefresh(refreshToken);
+    }
+
+    // JWT Refresh 토큰 기반 삭제 메소드
+    @Transactional
+    public void removeRefresh(String refreshToken) {
+
+        refreshRepository.deleteByRefresh(refreshToken);
+    }
+
+    // JWT 발급 memberId 기반 삭제 메소드 (탈퇴시)
+    public void removeRefreshUser(String memberId) {
+        
+        refreshRepository.deleteByMemberId(memberId);
+    }
+}
