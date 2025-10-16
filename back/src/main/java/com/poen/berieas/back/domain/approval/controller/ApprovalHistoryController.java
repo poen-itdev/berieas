3a7f2696b0fb89fff4d21a89523cb1236c33ba12
김우재 <wjkim@poen.co.kr>
2025-10-16 16:35:58 +0900

    api수정
    재직자 리스트

--- back/src/main/java/com/poen/berieas/back/domain/approval/controller/ApprovalHistoryController.java
+++ back/src/main/java/com/poen/berieas/back/domain/approval/controller/ApprovalHistoryController.java
@@ -2,7 +2,6 @@ package com.poen.berieas.back.domain.approval.controller;
 
 import java.util.List;
 
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
